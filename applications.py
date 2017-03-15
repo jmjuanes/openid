29dcc440f7c75ebe42e08f6dda019003d789132c
jmjuanes <josemijuanes@gmail.com>
2017-03-15 20:45:41 +0100

    applications.py: manage applications entity

---
+++ applications.py
@@ -0,0 +1,26 @@
+# Import google modules
+from google.appengine.ext import ndb
+
+
+# Applications class
+class Application(ndb.Model):
+    secret = ndb.StringProperty()
+    name = ndb.StringProperty()
+    detail = ndb.StringProperty()
+    redirect = ndb.StringProperty()
+
+
+# Get an application by id
+def get_application(value):
+    try:
+        # Convert the id to integer and return the application
+        value = int(value)
+        return Application.get_by_id(value)
+    except ValueError:
+        # Catch an error: return none and exit
+        return None
+
+
+# Check if an application exists
+def exists_application(value):
+    return get_application(value) is not None
