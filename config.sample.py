a6b358d418d6dc7caecf69c1a615c850ff76eb91
jmjuanes <josemijuanes@gmail.com>
2017-03-15 20:51:43 +0100

    config.sample.py: added sample configuration file

---
+++ config.sample.py
@@ -0,0 +1,20 @@
+# Sample configuration file
+# You must rename this file as "config.py" and edit the fields below with
+# your own configuration.
+# Check the documentation at https://github.com/mgviz/openid
+
+# Captcha configuration
+# ======================
+# Visit the ReCaptcha documentation at https://developers.google.com/recaptcha/intro
+
+# Captcha verification url
+# You must not change this url
+captcha_url = 'https://www.google.com/recaptcha/api/siteverify'
+
+# Captcha key
+# Enter your captcha key value
+captcha_key = 'YOUR_CAPTCHA_KEY'
+
+# Captcha secret key
+# Enter your captcha secret key value
+captcha_secret = 'YOUR_CAPTCHA_SECRET_KEY'
