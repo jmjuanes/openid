bb17844149c87015fae46efa8057949f35b67c56
jmjuanes <josemijuanes@gmail.com>
2017-04-15 18:57:21 +0200

    route_authorize.py: fixed bugs. Fixed redirect url

--- route_authorize.py
+++ route_authorize.py
@@ -16,24 +16,27 @@ import tokens
 
 # Authorize page
 class RouteAuthorize(webapp2.RequestHandler):
+    # Get action
     def get(self):
         # Get the application id
-        application_id = self.request.GET('client', '')
+        application_id = self.request.get('client', default_value='')
     
         # Get if signup is allowed
-        application_signup = self.request.GET('allow_signup', 'false')
+        application_signup = self.request.get('allow_signup', default_value='false')
     
         # Check for undefined application id
         if application_id == '':
-            return render.template(self, 'error.html', error='No application id provided'), 404
-    
+            self.response.status_int = 400
+            return render.template(self, 'error.html', error='No application id provided')
+
         # Get the application information
         application = db_application.get_application(application_id)
-    
+
         # Check if the application exists
         if application is None:
-            return render.template(self, 'error.html', error='Invalid application'), 404
-    
+            self.response.status_int = 404
+            return render.template(self, 'error.html', error='Invalid application')
+
         # Get the application state
         application_state = pbkdf2_sha256.hash(application_id)
     
@@ -44,34 +47,39 @@ class RouteAuthorize(webapp2.RequestHandler):
                                app_signup=application_signup,
                                app_name=application.name)
 
+    # Post action
     def post(self):
         # Get the user email and password
-        user_email = self.request.get('email', '')
-        user_pwd = self.request.get('pwd', '')
+        user_email = self.request.get('email', default_value='')
+        user_pwd = self.request.get('pwd', default_value='')
     
         # Check for empty email or password
         if user_email == '' or user_pwd == '':
-            return render.template(self, 'error.html', error='Invalid user request'), 400
+            self.response.status_int = 400
+            return render.template(self, 'error.html', error='Invalid user request')
     
         # Get the application id and state
-        application_id = self.request.get('client', '')
-        application_state = self.request.get('state', '')
-        application_signup = self.request.get('signup', '')
+        application_id = self.request.get('client', default_value='')
+        application_state = self.request.get('state', default_value='')
+        application_signup = self.request.get('signup', default_value='false')
     
         # Check for empty application id or state
         if application_id == '' or application_state == '':
-            return render.template(self, 'error.html', error='Invalid application request'), 400
+            self.response.status_int = 400
+            return render.template(self, 'error.html', error='Invalid application request')
     
         # Check if the request has been made by a third party application and the process should be aborted
         if pbkdf2_sha256.verify(application_id, application_state) is False:
-            return render.template(self, 'error.html', error='Invalid request state'), 400
+            self.response.status_int = 400
+            return render.template(self, 'error.html', error='Invalid request state')
     
         # Get the application
         application = db_application.get_application(application_id)
     
         # Check for application not found
         if application is None:
-            return render.template(self, 'error.html', error='Application not found'), 404
+            self.response.status_int = 400
+            return render.template(self, 'error.html', error='Application not found')
     
         # Check if the captcha is enabled
         if config.captcha_enabled is True:
@@ -80,6 +88,7 @@ class RouteAuthorize(webapp2.RequestHandler):
     
             # Check for empty captcha
             if captcha_value == '':
+                self.response.status_int = 400
                 return render.template(self, 'authorize.html',
                                        app_id=application_id,
                                        app_state=application_state,
@@ -89,6 +98,7 @@ class RouteAuthorize(webapp2.RequestHandler):
     
             # Check the captcha value
             if captcha.verify(captcha_value) is False:
+                self.response.status_int = 400
                 return render.template(self, 'authorize.html',
                                        app_id=application_id,
                                        app_state=application_state,
@@ -99,8 +109,9 @@ class RouteAuthorize(webapp2.RequestHandler):
         # Get the user
         user = db_user.get_user(user_email)
     
-        # Check for non user
+        # Check for no user
         if user is None:
+            self.response.status_int = 400
             return render.template(self, 'authorize.html',
                                    app_id=application_id,
                                    app_state=application_state,
@@ -110,6 +121,7 @@ class RouteAuthorize(webapp2.RequestHandler):
         else:
             # Check the password
             if pbkdf2_sha256.verify(user_pwd, user.pwd) is False:
+                self.response.status_int = 400
                 return render.template(self, 'authorize.html',
                                        app_id=application_id,
                                        app_state=application_state,
@@ -120,8 +132,9 @@ class RouteAuthorize(webapp2.RequestHandler):
                 # Build the token
                 user_token = tokens.encode(user, application.secret)
     
-                # Redirect to the site
-                return self.redirect(urlparse.urljoin(application.redirect, '?token=' + user_token))
+                # Build the outgoing url and redirect
+                redirect_url = urlparse.urljoin(application.redirect, '?token=' + user_token)
+                return self.redirect(str(redirect_url))
 
 
 # Initialize the app
