cfd49bf19b60e1ccd30c9458c426c366e670fc64
jmjuanes <josemijuanes@gmail.com>
2017-04-15 19:00:43 +0200

    route_register.py: added register route

---
+++ route_register.py
@@ -0,0 +1,62 @@
+# Import dependencies
+import webapp2
+
+# Import lib modules
+from passlib.hash import pbkdf2_sha256
+
+# Import modules
+import captcha
+import config
+import db_user
+import render
+
+
+# Main page
+class RouteRegister(webapp2.RequestHandler):
+    def get(self):
+        # Render the register page
+        return render.template(self, 'register.html', error='')
+
+    def post(self):
+        # Check if the captcha is enabled
+        if config.captcha_enabled is True:
+            # Get the captcha value
+            captcha_value = self.request.get('g-recaptcha-response', default_value='')
+
+            # Check the captcha value
+            if captcha.verify(captcha_value) is False:
+                self.response.status_int = 400
+                return render.template(self, 'register.html',  error='Invalid captcha')
+    
+        # Initialize the user
+        user = db_user.User()
+    
+        # Get the post values
+        user.name = self.request.get('name', '')
+        user.institution = self.request.get('institution', '')
+        user.email = self.request.get('email', '')
+        user.pwd = self.request.get('pwd', '')
+        user.is_admin = False  # By default is not admin
+    
+        # Encrypt the password
+        user.pwd = pbkdf2_sha256.hash(user.pwd)
+    
+        # Check the values
+        if user.name is '' or user.institution is '' or user.email is '' or user.pwd is '':
+            return render.template(self, 'register.html', error='You must fill all fields')
+    
+        # Check if the email is registered
+        if db_user.exists_user(user.email):
+            return render.template(self, 'register.html', error='The email has already registered')
+    
+        # Register the user
+        user_key = user.put()
+    
+        # Get the continue argument
+        # continue_url = request.args.get('continue', '')
+    
+        # Render the registration completed page
+        return render.template(self, 'register-completed.html')
+
+# Initialize the app
+app = webapp2.WSGIApplication([('/register', RouteRegister)], debug=True)
