76af210cb09fafefa2249df6ef7fecaf3a6df23c
jmjuanes <josemijuanes@gmail.com>
2017-04-15 14:01:53 +0200

    db.py: merge users and applications into db.py

--- db.py
+++ db.py
@@ -24,3 +24,29 @@ def get_application(value):
 # Check if an application exists
 def exists_application(value):
     return get_application(value) is not None
+
+
+# Users class
+class User(ndb.Model):
+    email = ndb.StringProperty()
+    name = ndb.StringProperty()
+    pwd = ndb.StringProperty()
+    institution = ndb.StringProperty()
+    is_admin = ndb.BooleanProperty()
+
+
+# Perform a query and get the users with the provided email
+def get_user(value):
+    # Get the users with this email
+    query = User.query(User.email == value)
+
+    # Check the number of users with this email
+    if query.count() != 1:
+        return None
+    else:
+        return query.get()
+
+
+# Check if an user with the provided email exists
+def exists_user(value):
+    return get_user(value) is not None
