c6fa9227821d2f250ba4061422702232335919e3
jmjuanes <josemijuanes@gmail.com>
2018-01-08 19:53:29 +0100

    Rename template rendering function as render. Minor changes

--- openid.py
+++ openid.py
@@ -24,7 +24,7 @@ jinja_env.globals['captcha_key'] = config.captcha_key
 
 
 # Render a template
-def template(self, file_name, **kwargs):
+def render(self, file_name, **kwargs):
     # Change the response headers
     self.response.headers['Content-Type'] = 'text/html'
 
@@ -36,7 +36,7 @@ def template(self, file_name, **kwargs):
 # Login route
 class RouteLogin(webapp2.RequestHandler):
     def get(self):
-        return template(self, 'login.html')
+        return render(self, 'login.html')
 
     def post(self):
         # Get the user email and password
@@ -46,7 +46,7 @@ class RouteLogin(webapp2.RequestHandler):
         # Check for empty email or password
         if user_email == '' or user_pwd == '':
             self.response.status_int = 400
-            return template(self, 'error.html', error='Invalid user request')
+            return render(self, 'error.html', error='Invalid user request')
 
         # logging.info('Logging user ' + user_email)
 
@@ -56,18 +56,18 @@ class RouteLogin(webapp2.RequestHandler):
             captcha_value = self.request.get('g-recaptcha-response', default_value='')
             if captcha.verify(captcha_value, config.captcha_secret, config.captcha_url) is False:
                 self.response.status_int = 400
-                return template(self, 'login.html',  error='Invalid captcha')
+                return render(self, 'login.html',  error='Invalid captcha')
 
         # Get the user from the database
         user = db_user.get_user(user_email)
         if user is None:
             self.response.status_int = 400
-            return template(self, 'login.html', error='Email not found')
+            return render(self, 'login.html', error='Email not found')
 
         # Check if user is active
         if user.active is False:
             self.response.status_int = 401
-            return template(self, 'login.html', error='User is not active')
+            return render(self, 'login.html', error='User is not active')
 
         # Check the password
         if pbkdf2_sha256.verify(user_pwd, user.pwd) is True:
@@ -76,7 +76,7 @@ class RouteLogin(webapp2.RequestHandler):
             self.redirect('/')
         else:
             self.response.status_int = 400
-            return template(self, 'login.html', error='Invalid email or password')
+            return render(self, 'login.html', error='Invalid email or password')
 
 
 # Register a new user route
@@ -85,7 +85,7 @@ class RouteRegister(webapp2.RequestHandler):
         # Check if signup is allowed
         if config.openid_allow_signup is False:
             return self.redirect('/login')
-        return template(self, 'register.html', error='')
+        return render(self, 'register.html', error='')
 
     def post(self):
         # Check if the signup is not allowed
@@ -108,7 +108,7 @@ class RouteRegister(webapp2.RequestHandler):
         # Check the values
         if user.name is '' or user.email is '' or user.pwd is '':
             self.response.status_int = 400
-            return template(self, 'register.html', error='You must fill all fields')
+            return render(self, 'register.html', error='You must fill all fields')
 
         # Check if the captcha is enabled
         if config.captcha_enabled is True:
@@ -118,20 +118,20 @@ class RouteRegister(webapp2.RequestHandler):
             # Check the captcha value
             if captcha.verify(captcha_value, config.captcha_secret, config.captcha_url) is False:
                 self.response.status_int = 400
-                return template(self, 'register.html',  error='Invalid captcha')
+                return render(self, 'register.html',  error='Invalid captcha')
 
         # Check if the email is registered
         if db_user.exists_user(user.email):
             self.response.status_int = 400
-            return template(self, 'register.html', error='The email has already registered')
+            return render(self, 'register.html', error='The email has already registered')
 
         # Register the user
         try:
             user_key = user.put()
             # continue_url = request.args.get('continue', '')
-            return template(self, 'register-completed.html')
+            return render(self, 'register-completed.html')
         except:
-            return template(self, 'register.html', error='Error creating the user. Contact with the administrator')
+            return render(self, 'register.html', error='Error creating the user. Contact with the administrator')
 
 
 # Render the authorize page
@@ -144,18 +144,18 @@ class RouteAuthorize(webapp2.RequestHandler):
         # Check for undefined application id
         if application_id == '':
             self.response.status_int = 400
-            return template(self, 'error.html', error='No application id provided')
+            return render(self, 'error.html', error='No application id provided')
 
         # Get the application information and check if exists
         application = db_application.get_application(application_id)
         if application is not None:
-            return template(self, 'authorize.html',
-                            app_id=application_id,
-                            app_signup=application_signup,
-                            app_name=application.name)
+            return render(self, 'authorize.html',
+                          app_id=application_id,
+                          app_signup=application_signup,
+                          app_name=application.name)
         else:
             self.response.status_int = 404
-            return template(self, 'error.html', error='Invalid application')
+            return render(self, 'error.html', error='Invalid application')
 
     def post(self):
         # Get the user email and password
@@ -165,7 +165,7 @@ class RouteAuthorize(webapp2.RequestHandler):
         # Check for empty email or password
         if user_email == '' or user_pwd == '':
             self.response.status_int = 400
-            return template(self, 'error.html', error='Invalid user request')
+            return render(self, 'error.html', error='Invalid user request')
 
         # Get the application id and state
         application_id = self.request.get('client', default_value='')
@@ -174,7 +174,7 @@ class RouteAuthorize(webapp2.RequestHandler):
         # Check for empty application id
         if application_id == '':
             self.response.status_int = 400
-            return template(self, 'error.html', error='Invalid application request')
+            return render(self, 'error.html', error='Invalid application request')
 
         # Get the application
         application = db_application.get_application(application_id)
@@ -182,7 +182,7 @@ class RouteAuthorize(webapp2.RequestHandler):
         # Check for application not found
         if application is None:
             self.response.status_int = 400
-            return template(self, 'error.html', error='Application not found')
+            return render(self, 'error.html', error='Application not found')
 
         # Check if the captcha is enabled
         if config.captcha_enabled is True:
@@ -192,11 +192,11 @@ class RouteAuthorize(webapp2.RequestHandler):
             # Check the captcha value
             if captcha.verify(captcha_value) is False:
                 self.response.status_int = 400
-                return template(self, 'authorize.html',
-                                app_id=application_id,
-                                app_signup=application_signup,
-                                app_name=application.name,
-                                error='Invalid captcha')
+                return render(self, 'authorize.html',
+                              app_id=application_id,
+                              app_signup=application_signup,
+                              app_name=application.name,
+                              error='Invalid captcha')
 
         # Get the user
         user = db_user.get_user(user_email)
@@ -204,29 +204,29 @@ class RouteAuthorize(webapp2.RequestHandler):
         # Check for no user
         if user is None:
             self.response.status_int = 400
-            return template(self, 'authorize.html',
-                            app_id=application_id,
-                            app_signup=application_signup,
-                            app_name=application.name,
-                            error='Email not found')
+            return render(self, 'authorize.html',
+                          app_id=application_id,
+                          app_signup=application_signup,
+                          app_name=application.name,
+                          error='Email not found')
         else:
             # Check if user is active
             if user.active is False:
                 self.response.status_int = 401
-                return template(self, 'authorize.html',
-                                app_id=application_id,
-                                app_signup=application_signup,
-                                app_name=application.name,
-                                error='User is not active')
+                return render(self, 'authorize.html',
+                              app_id=application_id,
+                              app_signup=application_signup,
+                              app_name=application.name,
+                              error='User is not active')
 
             # Check the password
             if pbkdf2_sha256.verify(user_pwd, user.pwd) is False:
                 self.response.status_int = 400
-                return template(self, 'authorize.html',
-                                app_id=application_id,
-                                app_signup=application_signup,
-                                app_name=application.name,
-                                error='Invalid email or password')
+                return render(self, 'authorize.html',
+                              app_id=application_id,
+                              app_signup=application_signup,
+                              app_name=application.name,
+                              error='Invalid email or password')
             else:
                 # Build the token and the outgoing url
                 user_token = tokens.encode(user, application.secret)
