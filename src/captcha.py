1871b2363e82fedc59513443e93882351ecc4c6c
jmjuanes <josemijuanes@gmail.com>
2018-01-08 18:35:07 +0100

    Add captcha secret and captcha url as function arguments. Remove config import

--- src/captcha.py
+++ src/captcha.py
@@ -1,54 +1,33 @@
-# Import general modules
 import logging
 import urllib
 import json
 
-# Import google modules
 from google.appengine.api import urlfetch
-
-# Import configuration
-import config
 
 
 # Verify captcha
-def verify(captcha_response):
-    # Check if captcha is enabled
-    if config.captcha_enabled is False:
-        return True
-
+def verify(captcha_response, captcha_secret, captcha_url):
     # Check for empty captcha response
     if captcha_response is '':
         return False
 
-    # Build the form fields
-    form_fields = {'secret': config.captcha_secret, 'response': captcha_response}
-
-    # Display in logs
-    logging.info('Checking captcha...')
-
-    # Fetch the url
+    # Perform the request to the captcha server
     try:
-        # Encode the form fields
+        form_fields = {'secret': captcha_secret, 'response': captcha_response}
         form_data = urllib.urlencode(form_fields)
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
         # Perform the request
-        result = urlfetch.fetch(url=config.captcha_url, payload=form_data, method=urlfetch.POST, headers=headers)
-
-        # Display in logs
-        logging.info('Captcha response code: ' + str(result.status_code) + '')
+        result = urlfetch.fetch(url=captcha_url, payload=form_data, method=urlfetch.POST, headers=headers)
+        # logging.info('Captcha response code: ' + str(result.status_code) + '')
 
         # Check the result status code
         if result.status_code == 200:
-            # Parse the response json and return if the captcha is valid
             data = json.loads(result.content)
             return data['success']
         else:
-            # Print error in logs and exit
             logging.error('Error response for verify captcha')
             return False
     except urlfetch.Error:
-        # Print error
         logging.exception('Caught exception fetching url for verify captcha')
         return False
-
