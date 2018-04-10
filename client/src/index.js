2e48b2fe2e13bdd9414fc94d9c1507dcca294f0a
jmjuanes <josemijuanes@gmail.com>
2018-04-10 16:28:48 +0200

    Init app

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,22 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import {hyperscript as h, ready} from "neutrine-utils";
+import * as Router from "neutrine-router";
+
+//Main app class
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return h("div", {}, "Hello world!");
+    }
+}
+
+//Load when dom is ready
+ready(function () {
+    //Mount the app component 
+    ReactDOM.render(h(App, {}), document.getElementById("root"));
+});
+
