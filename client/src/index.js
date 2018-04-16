75348a1a4e17d0df2b34293c04b7f1427b70b982
alextjd <alextjd1@gmail.com>
2018-04-16 11:59:21 +0200

    Minor changes.

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,30 @@ import ReactDOM from "react-dom";
 import {hyperscript as h, ready} from "neutrine-utils";
 import * as Router from "neutrine-router";
 
+import Login from "./pages/login/index.js";
+
+//General props
+let defaultInfo = {
+    openid_name: "OPENID_NAME",
+    captcha: false,
+    captcha_key: 'nope',
+    openid_allow_signup: true,
+    // error: "This is an error"
+};
+
 //Main app class
 class App extends React.Component {
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        return h("div", {}, "Hello world!");
+        return (
+            <Router.Switch>
+                <Router.Route exact path="/" component={Login} props={defaultInfo}/>
+            </Router.Switch>
+        );
     }
 }
 
