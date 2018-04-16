4ec8005e3bc938a7ae8c0a3b8aba711a868e14d7
jmjuanes <josemijuanes@gmail.com>
2018-04-16 13:14:32 +0200

    Added refs to inputs. Added sign-in click listener to send the request to the api's login route

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,22 +1,58 @@
 import React from "react";
-
 import {Heading,Alert, Field, FieldLabel, FieldHelper, Input, Btn} from "neutrine";
+import {request} from "neutrine-utils";
+
 import "./styles.scss";
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: props.error
+            error: null 
+        };
+        this.ref = {
+            emailInput: React.createRef(),
+            pwdInput: React.createRef()
+        };
+
+        //Bind methods 
+        this.handleSignInClick = this.handleSignInClick.bind(this);
+    }
+
+    handleSignInClick() {
+        let self = this;
+        //console.log(this.ref.emailInput.current.value);
+        //console.log(this.ref.pwdInput.current.value);
+        let credentials = {
+            email: "",
+            pwd: ""
+        };
+        //Check for a valid email
+        if (credentials.email.indexOf("@") === -1) {
+            return this.setState({error: "Invalid email provided"});
         }
+        //Check the password
+        if (credentials.pwd.length < 6) {
+            return this.setState({error: "Invalid password"});
+        }
+        //Do the request
+        request({url: "/api/login", method: "post", json: true, body: credentials}), function (error, res, body) {
+            if (error) {
+                return self.setState({error: error.message});
+            }
+            if (res.statusCode >= 300) {
+                return self.setState({error: body.message});
+            }
+            return self.props.saveToken(body.token);
+        });
     }
 
     // Display the error message
     renderError() {
-        if (this.props.error) {
+        if (this.state.error) {
             return (
                 <Alert color={"red"} className={"login-error"}>
-                    {this.props.error}
+                    {this.state.error}
                 </Alert>
             )
         }
@@ -63,14 +99,19 @@ class Login extends React.Component {
                     {/*Email*/}
                     <Field>
                         <FieldLabel>Email</FieldLabel>
-                        <Input className="login-input"/>
+                        <Input className="login-input"
+                            ref={this.ref.emailInput}/>
                         <FieldHelper>Please enter a valid email</FieldHelper>
                     </Field>
                     {/*Password input*/}
                     <Field>
                         <FieldLabel>Password</FieldLabel>
-                        <Input className="login-input" type={"password"} pattern={".{6,}"}
-                               title={"6 characters minimum"} name={"pwd"}/>
+                        <Input className="login-input" 
+                            ref={this.ref.pwdInput}
+                            type={"password"} 
+                            pattern={".{6,}"}
+                            title={"6 characters minimum"} 
+                            name={"pwd"}/>
                     </Field>
                     {/*Captcha*/}
                     {this.renderCaptcha()}
@@ -79,7 +120,7 @@ class Login extends React.Component {
                         Check if all the information is correct and click on <b>"Sign in"</b>
                     </div>
                     {/*Sign in*/}
-                    <Btn color={"blue"} fluid>Sign in</Btn>
+                    <Btn color={"blue"} fluid onClick={this.handleSignInClick}>Sign in</Btn>
                     {/*New account*/}
                     {this.renderRegisterField()}
                 </form>
