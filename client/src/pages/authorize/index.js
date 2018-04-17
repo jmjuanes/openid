0a34f4fadd9e2ff8d1f18704d7f9224062d05023
alextjd <alextjd1@gmail.com>
2018-04-17 13:38:31 +0200

    Implemented the onclick. Still does not work (pending to see how query parameter works).

--- client/src/pages/authorize/index.js
+++ client/src/pages/authorize/index.js
@@ -1,17 +1,56 @@
 import React from "react";
 import {Btn, Field, FieldHelper, FieldLabel, Heading, Input} from 'neutrine';
+import {request} from "neutrine-utils";
 
 class Authorize extends React.Component {
     constructor(props) {
         super(props);
+        // Check if the error
+        let err = Object.keys(props.request.query).length === 0 ? "Invalid application id" : null;
         this.state = {
             app_name: props.app_name,
-            error: null
+            error: err
         };
         this.ref = {
             emailInput: React.createRef(),
             pwdInput: React.createRef()
         };
+        // Bind functions
+        this.handleAuthorizeClick = this.handleAuthorizeClick.bind(this);
+    }
+
+    // Authorize
+    handleAuthorizeClick() {
+        let self = this;
+        let credentials = {
+            email: this.ref.emailInput.current.value,
+            pwd: this.ref.pwdInput.current.value,
+            client: this.props.request.query.id
+        };
+        // Check if the email is valid
+        if (credentials.email.indexOf("@") === -1) {
+            return this.setState({error: "Invalid email provided"});
+        }
+        // Check if the password is valid
+        if (credentials.pwd.length < 6) {
+            return this.setState({error: "Invalid password"});
+        }
+        // Check if the application id exists
+        if (typeof credentials.client === "undefined" || credentials.client.length === 0) {
+            return this.setState({error: "Invalid application id"});
+        }
+
+        // Do the request
+        request({url: "/api/authorize", method: "post", json: true, body: credentials}, function (err, res, body) {
+            if(err){
+                return self.setState({error: err.message});
+            }
+            if(res.statusCode >= 300){
+                return self.setState({error: body.message})
+            }
+            // return;
+        })
+
     }
 
     // Display the error message
@@ -31,7 +70,7 @@ class Authorize extends React.Component {
             return (
                 <div className="login-captcha">
                     <label className="siimple-label">Are you human?</label><br/>
-                    <div className="g-recaptcha" data-sitekey={this.props.captcha_key}> </div>
+                    <div className="g-recaptcha" data-sitekey={this.props.captcha_key}></div>
                 </div>
             );
         }
@@ -55,6 +94,7 @@ class Authorize extends React.Component {
                     <Field>
                         <FieldLabel>Email</FieldLabel>
                         <Input className="authorize-input"
+                               inputRef={this.ref.emailInput}
                                required/>
                         <FieldHelper>Please enter a valid email</FieldHelper>
                     </Field>
@@ -63,6 +103,7 @@ class Authorize extends React.Component {
                         <FieldLabel>Password</FieldLabel>
                         <Input className="authorize-input"
                                type={"password"}
+                               inputRef={this.ref.pwdInput}
                                required/>
                     </Field>
                     {/*Captcha*/}
@@ -72,7 +113,7 @@ class Authorize extends React.Component {
                         Check if all the information is correct and click on <b>"Sign in"</b>
                     </div>
                     {/*Submit the information*/}
-                    <Btn color={"blue"} fluid>Authorize</Btn>
+                    <Btn color={"blue"} onClick={this.handleAuthorizeClick} fluid>Authorize</Btn>
                 </div>
             </div>
         );
