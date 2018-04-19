6a8adb1cbe38c7496b909cce6249d45d81e5e981
alextjd <alextjd1@gmail.com>
2018-04-19 13:23:39 +0200

    Functionality completed: the user is registered successfully.

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Alert, Btn, Field, FieldLabel, Heading, Input} from "neutrine";
+import {Alert, Btn, Field, FieldHelper, FieldLabel, Heading, Input} from "neutrine";
+import {request} from "neutrine-utils";
 
 import './styles.scss';
-
 
 
 class Register extends React.Component {
@@ -11,6 +11,14 @@ class Register extends React.Component {
         this.state = {
             error: null
         };
+        this.ref = {
+            nameInput: React.createRef(),
+            emailInput: React.createRef(),
+            pwdInput: React.createRef(),
+            pwdRepeatInput: React.createRef()
+        };
+        //Bind functions
+        this.handleRegisterClick = this.handleRegisterClick.bind(this);
     }
 
     // Display the error message
@@ -36,6 +44,40 @@ class Register extends React.Component {
         }
     }
 
+    // Register the user with the provided info
+    handleRegisterClick() {
+        let self = this;
+        let credentials = {
+            name: this.ref.nameInput.current.value,
+            email: this.ref.emailInput.current.value,
+            pwd: this.ref.pwdInput.current.value,
+        };
+        // Check if the email is valid
+        if (credentials.email.indexOf("@") === -1) {
+            return this.state.setState({error: "Invalid email provided"});
+        }
+        // Check if the password is valid
+        if (credentials.pwd.length < 6) {
+            return this.setState({error: "Invalid password"})
+        }
+        // Check if passwords match
+        if (credentials.pwd !== this.ref.pwdRepeatInput.current.value) {
+            return this.setState({error: "Passwords don't match"});
+        }
+
+        // Do the request
+        request({url: "/api/users", method: "post", json: true, body: credentials},
+            function (err, res, body) {
+                if (err) {
+                    self.setState({error: err.message});
+                }
+                if(res.statusCode >= 300){
+                    self.setState({error: body.message});
+                }
+                return window.location.replace("http://localhost:8080/#!/login");
+            });
+    }
+
     render() {
         return (
             <div className={"register-content"}>
@@ -43,7 +85,8 @@ class Register extends React.Component {
                 <Heading type={"h2"} align="center">Register</Heading>
                 {/*Subtitle*/}
                 <div className={"register-subtitle siimple-small"} align={"center"}>
-                    Fill the following fields to create a new <b>{this.props.openid_name}</b> account. All of them are required.
+                    Fill the following fields to create a new <b>{this.props.openid_name}</b> account. All of them are
+                    required.
                 </div>
                 {/*Form*/}
                 <div className={"register-form"}>
@@ -53,29 +96,31 @@ class Register extends React.Component {
                     <Field>
                         <FieldLabel>Name</FieldLabel>
                         <Input className="register-input"
-                            // inputRef={this.ref.nameInput}
+                               inputRef={this.ref.nameInput}
+                               maxLength={"16"}
                                required/>
                     </Field>
                     {/*Email input*/}
                     <Field>
                         <FieldLabel>Email</FieldLabel>
                         <Input className="register-input"
-                            // inputRef={this.ref.emailInput}
+                               inputRef={this.ref.emailInput}
                                required/>
                     </Field>
                     {/*Password input*/}
                     <Field>
                         <FieldLabel>Password</FieldLabel>
                         <Input className="register-input"
-                            // inputRef={this.ref.pwdInput}
+                               inputRef={this.ref.pwdInput}
                                type={"password"}
                                required/>
+                        <FieldHelper>6 characters minimum</FieldHelper>
                     </Field>
                     {/*Password input*/}
                     <Field>
                         <FieldLabel>Repeat password</FieldLabel>
                         <Input className="register-input"
-                            // inputRef={this.ref.pwdRepeatInput}
+                               inputRef={this.ref.pwdRepeatInput}
                                type={"password"}
                                required/>
                     </Field>
@@ -86,7 +131,14 @@ class Register extends React.Component {
                         Check that all the information is correct and click on <b>"Create account"</b>
                     </div>
                     {/*Register*/}
-                    <Btn color={"blue"} fluid>Create account</Btn>
+                    <Btn color={"blue"} onClick={this.handleRegisterClick} fluid>Create account</Btn>
+                    {/*Login*/}
+                    <Field className={"register-login"}>
+                        <FieldLabel align="center">
+                            Are you already registered?
+                        </FieldLabel>
+                        <Btn color={"green"} fluid> Log in </Btn>
+                    </Field>
                 </div>
             </div>
         );
