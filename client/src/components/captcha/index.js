e4e53a04c3ae77dcc236f9c5b0b85ff8e5ba6906
jmjuanes <josemijuanes@gmail.com>
2018-04-24 10:56:59 +0200

    Added setInterval to check when grecaptcha is available

--- client/src/components/captcha/index.js
+++ client/src/components/captcha/index.js
@@ -1,22 +1,50 @@
 import React from "react";
+import {Spinner} from "neutrine";
+
+//Check if recaptcha is ready 
+let recpatchaReady = function () {
+    return typeof window !== "undefined" && typeof window.grecaptcha !== "undefined";
+};
 
 //Export the captcha component
 export default class Captcha extends React.Component {
     constructor(props) {
         super(props);
+        //Initial state 
+        this.state = {
+            ready: recaptchaReady()
+        };
         //Referenced elements
         this.ref = {
             "wrapper": React.createRef(),
-            "captcha": null
+            "captcha": null,
+            "readyCheck": null
         };
         //Bind listeners
         this.handleResponse = this.handleResponse.bind(this);
         this.handleExpired = this.handleExpired.bind(this);
         this.handleError = this.handleError.bind(this);
+        //Check if the recaptcha is not ready to use 
+        if (this.state.ready === false) {
+            let self = this;
+            this.ref.readyCheck = setInterval(function () {
+                //Check if recaptcha is ready
+                let isReady = recaptchaReady();
+                if (isReady === true) {
+                    clearInterval(self.ref.readyCheck);
+                    self.setState({ready: true});
+                }
+            }, 500);
+        }
     }
 
     componentDidMount() {
         return this.renderCaptcha();
+    }
+
+    componentWillUnmount() {
+        //Clear the ready check interval
+        clearInterval(this.ref.readyCheck);
     }
 
     componentDidUpdate() {
@@ -25,16 +53,19 @@ export default class Captcha extends React.Component {
 
     renderCaptcha() {
         let self = this;
-        //Render the captcha and save the captcha widget
-        this.ref.captcha = grecaptcha.render(this.ref.wrapper.current, {
-            "sitekey": self.props.sitekey,
-            "theme": self.props.theme,
-            "size": self.props.size,
-            "tabindex": self.props.tabindex,
-            "callback": self.handleResponse,
-            "expired-callback": self.handleExpired,
-            "error-callback": self.handleError
-        });
+        //Check if the captcha is ready
+        if (this.state.ready === false) {
+            //Render the captcha and save the captcha widget
+            this.ref.captcha = grecaptcha.render(this.ref.wrapper.current, {
+                "sitekey": self.props.sitekey,
+                "theme": self.props.theme,
+                "size": self.props.size,
+                "tabindex": self.props.tabindex,
+                "callback": self.handleResponse,
+                "expired-callback": self.handleExpired,
+                "error-callback": self.handleError
+            });
+        }
     }
 
     getResponse() {
@@ -65,8 +96,13 @@ export default class Captcha extends React.Component {
 
     render() {
         let self = this;
+        let children = null;
+        //Check if the captcha is ready 
+        if (this.state.ready === false) {
+            children = React.createElement(Spinner, {color: "blue"});
+        }
         //Return the captcha div wrapper
-        return React.createElement("div", {ref: self.ref.wrapper});
+        return React.createElement("div", {ref: self.ref.wrapper, align: "center"}, children);
     }
 }
 
