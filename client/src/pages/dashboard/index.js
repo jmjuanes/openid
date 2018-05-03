1427216307e621e72e069b83333adb4e28c8d5e6
alextjd <alextjd1@gmail.com>
2018-05-03 11:33:37 +0200

    Updated the request method with correct auth token.

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,59 +1,80 @@
 import React from "react";
+import * as Router from "rouct";
 
 import "./styles.scss";
 import Profile from "./profile/index";
+import {List, ListItem, ListTitle, Spinner} from "neutrine";
+import {request} from "neutrine-utils";
 
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
-
-        // Bind functions
-        this.renderAdminPanel = this.renderAdminPanel.bind(this);
-    }
-
-    renderAdminPanel() {
-        if (this.props.is_admin === true) {
-            return (
-                // Admin panel
-                <div className="dash-admin-panel">
-                    <div className="siimple-menu">
-                        <div className="siimple-menu-group">Administration</div>
-                        <a className="siimple-menu-item" href="#">Users management</a>
-                        <a className="siimple-menu-item" href="#">Apps management</a>
-                    </div>
-                </div>
-            );
+        this.state = {
+            ready: false,
+            user: {}
         }
     }
 
+    // Get the user info using his access token
+    componentDidMount() {
+        // Change to dashboard
+        let self = this;
+        // let author_header = "Bearer " + this.props.token;
+
+        request({
+            url: "/api/user",
+            method: "get",
+            json: true,
+            auth: {bearer: this.props.token}
+        }, function (err, res, body) {
+            if (err) {
+                return self.setState({error: err.message});
+            }
+            if (res.statusCode >= 300) {
+                return self.setState({error: body.message});
+            }
+            return self.setState({
+                user: {
+                    id: body.id
+                },
+                ready: true
+            });
+        });
+    }
+
     render() {
-        return (
-            <div className="siimple-content siimple-content--small">
-                <div className="siimple-grid">
-                    <div className="siimple-grid-row">
-                        {/*Side menu*/}
-                        <div className="dash-menu siimple-grid-col siimple-grid-col--3">
-                            {/*User panel*/}
-                            <div className="dash-user-panel">
-                                <div className="siimple-menu">
-                                    <div className="siimple-menu-group">Personal settings</div>
-                                    <a className="siimple-menu-item" href="#">Profile</a>
-                                    <a className="siimple-menu-item">Authorised apps</a>
-                                    <a className="siimple-menu-item" href="#">Change password</a>
-                                    <a className="siimple-menu-item" href="#">Delete account</a>
-                                </div>
+        if (this.state.ready === false) {
+            return (
+                <Spinner className={"dash-loading"}/>
+            );
+        }
+        else
+            return (
+                <div className="siimple-content siimple-content--medium">
+                    <div className="siimple-grid">
+                        <div className="siimple-grid-row">
+                            {/*Side menu*/}
+                            <div className="dash-menu siimple-grid-col siimple-grid-col--3">
+                                {/*User panel*/}
+                                <List>
+                                    <ListItem>Profile</ListItem>
+                                    <ListItem>Authorised apps</ListItem>
+                                    <ListItem>Change password</ListItem>
+                                    <ListItem>Delete account</ListItem>
+                                </List>
                             </div>
-                            {/*Admin panel*/}
-                            {this.renderAdminPanel()}
-                        </div>
-                        {/*Content*/}
-                        <div className="dash-content siimple-grid-col siimple-grid-col--9">
-                            <Profile token={this.props.token}/>
+                            {/*Content*/}
+                            <div className="dash-content siimple-grid-col siimple-grid-col--9">
+                                <Router.Switch>
+                                    <Router.Route exact path={"/dashboard/"}
+                                                  component={Profile}
+                                                  props={{token: this.props.token}}/>
+                                </Router.Switch>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
     }
 
 }
