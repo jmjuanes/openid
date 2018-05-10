e9731955ce04c024bfb4ef974012074cddae49c0
alextjd <alextjd1@gmail.com>
2018-05-10 12:17:44 +0200

    Implemented the updateApp successfully.

--- client/src/pages/dashboard/applications/edit/index.js
+++ client/src/pages/dashboard/applications/edit/index.js
@@ -20,6 +20,7 @@ class EditApp extends Component {
         };
 
         this.renderAlert = this.renderAlert.bind(this);
+        this.updateApp = this.updateApp.bind(this);
     }
 
     componentWillMount() {
@@ -28,10 +29,10 @@ class EditApp extends Component {
         let url = "/api/applications/" + this.props.request.params.id;
         request({url: url, method: "get", json: true, auth: {bearer: this.props.token}}, function (err, res, body) {
             if (err) {
-                return self.setState({error: err.message});
+                return self.setState({error: err.message, done: null});
             }
             if (res.statusCode >= 300) {
-                return self.setState({error: body.message});
+                return self.setState({error: body.message, done: null});
             }
             self.setState({
                 app: {
@@ -40,6 +41,50 @@ class EditApp extends Component {
                     redirect: body.redirect
                 },
                 ready: true
+            });
+        });
+    }
+
+    // Update the application info
+    updateApp() {
+        let self = this;
+        let info = {
+            name: this.ref.nameInput.current.value,
+            detail: this.ref.detailInput.current.value,
+            redirect: this.ref.redirectInput.current.value
+        };
+        // Check that all the info is new
+        if (info.name === this.state.app.name && info.detail === this.state.app.detail && info.redirect === this.state.app.redirect) {
+            return this.setState({error: "Change the app information before submitting", done: null});
+        }
+        // Check that there aren't empty fields
+        if (info.name.length === 0 || info.detail.length === 0 || info.redirect.length === 0) {
+            return this.setState({error: "No field can be empty", done: null});
+        }
+
+        let url = "/api/applications/" + this.props.request.params.id;
+        request({
+            url: url,
+            method: "put",
+            json: true,
+            body: info,
+            auth: {bearer: this.props.token}
+        }, function (err, res, body) {
+            if (err) {
+                return self.setState({error: err.message, done: null});
+            }
+            if (res.statusCode >= 300) {
+                return self.setState({error: body.message, done: null});
+            }
+            // Set the new info
+            return self.setState({
+                app: {
+                    name: body.name,
+                    detail: body.detail,
+                    redirect: body.redirect
+                },
+                done: "Application information successfully updated",
+                error: null
             });
         });
     }
@@ -98,7 +143,9 @@ class EditApp extends Component {
                                    defaultValue={this.state.app.redirect}
                                    inputRef={this.ref.redirectInput}/>
                         </Field>
-                        <Btn color={"blue"}>Update application</Btn>
+                        <Btn color={"blue"} onClick={() => {
+                            this.updateApp();
+                        }}>Update application</Btn>
                     </div>
                 </div>
             );
