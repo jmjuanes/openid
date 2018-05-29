22a5b8b5d64d26bc1d51141f7e79fe395ddea2b6
alextjd <alextjd1@gmail.com>
2018-05-29 20:21:08 +0000

    Reformat component. Minor changes

--- client/src/pages/dashboard/applications/index.js
+++ client/src/pages/dashboard/applications/index.js
@@ -2,19 +2,22 @@ import React from "react";
 import {Alert, Btn, Heading, Input, Spinner, Small, Paragraph} from "neutrine";
 import {request} from "@kofijs/request";
 import {redirectHashbang as redirect} from "rouct";
-import * as notification from "../../../commons/notification.js";
 
 import Subhead from "../../../components/subhead/index.js";
 import Table from "../../../components/table/index.js";
 
+import * as auth from "../../../commons/auth.js";
+import * as notification from "../../../commons/notification.js";
+
 import "./styles.scss";
 
-class Applications extends React.Component {
+//Applications list component
+export default class Applications extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            applications: [],
-            ready: false
+            "applications": [],
+            "loading": true
         };
         this.listApplications = this.listApplications.bind(this);
         this.editRedirect = this.editRedirect.bind(this);
@@ -23,74 +26,81 @@ class Applications extends React.Component {
 
     componentDidMount() {
         let self = this;
-        request({url: "/api/applications", method: "get", json: true, auth: {bearer: localStorage.getItem("token")}},
-            function (err, res, body) {
-                if (err) {
-                    return notification.error(err.message);
-                }
-                if (res.statusCode >= 300) {
-                    return notification.error(body.message);
-                }
-                return self.setState({applications: body.applications, ready: true});
+        let requestOptions = {
+            "url": "/api/applications",
+            "method": "get",
+            "json": true,
+            "auth": auth.generateAuth()
+        };
+        //Import the list with all the applications
+        return request(requestOptions, function (err, res, body) {
+            if (err) {
+                return notification.error(err.message);
+            }
+            if (res.statusCode >= 300) {
+                return notification.error(body.message);
+            }
+            //Update the state
+            return self.setState({
+                "applications": body.applications, 
+                "loading": true
             });
+        });
     }
 
-    // Redirect the admin to edit the specific application
+    //Redirect the admin to edit the specific application
     editRedirect(item) {
-        let url = "/dashboard/applications/" + item.id;
-        return redirect(url);
+        return redirect("/dashboard/applications/" + item.id);
     }
 
-    // Redirect the user to edit the specific application
+    //Redirect the user to edit the specific application
     createRedirect() {
         return redirect("/dashboard/applications/create");
     }
 
-    // Render a list with all the applications
-    listApplications() {
-        if (this.state.ready) {
-            let customTitle = function (item) {
-                return item.name;
-            };
-            let customDetail = function (item) {
-                return "Application created on xxxxxxxxx";
-            };
-            // Render the table
-            return (
-                <div className="apps-list">
-                    <Table data={this.state.applications} 
-                        icon="application" 
-                        actionText="Manage"
-                        onActionClick={this.editRedirect}
-                        customTitle={customTitle} 
-                        customDetail={customDetail}/>
-                </div>
-            );
-        }
+    //Render a list with all the applications
+    renderApplications() {
+        //Check for loading
+        if (this.state.loading === true) {
+            return (<Spinner color="primary"/>);
+        } 
+        let customTitle = function (item) {
+            return item.name;
+        };
+        let customDetail = function (item) {
+            return "Application created on xxxxxxxxx";
+        };
+        //Render the table
+        return (
+            <div className="apps-list">
+                {/* Display the number of registered applications */}
+                <Paragraph>
+                    You have <strong>{this.state.applications.length}</strong> applications registered.
+                </Paragraph>
+                {/* Display the table with all the applications */}
+                <Table data={this.state.applications} 
+                    icon="application" 
+                    actionText="Manage"
+                    onActionClick={this.editRedirect}
+                    customTitle={customTitle} 
+                    customDetail={customDetail}/>
+            </div>
+        );
     }
 
     render() {
-        if(!this.props.admin){
-            return(
-                <Alert>You must be an administrator to access this route.</Alert>
-            );
+        //Check if the user is an administrator
+        if (this.props.admin === false) {
+            return(<Alert>You must be an administrator to access this route.</Alert>);
         }
-        else {
-            if (!this.state.ready) {
-                return (<Spinner/>);
-            }
-            else
-                return (
-                    <div className="applications-content">
-                        <Subhead headerText="Applications" btnText="New Application" onBtnClick={() => this.createRedirect()}/>
-                        <Paragraph>
-                            You have <strong>{this.state.applications.length}</strong> applications registered. 
-                        </Paragraph>
-                        {this.listApplications()}
-                    </div>
-                );
-        }
+        return (
+            <div>
+                {/* Subheader with the button to register a new application */}
+                <Subhead headerText="Applications" btnText="New Application" onBtnClick={() => this.createRedirect()}/>
+                {/* Render the table with all the applications */}
+                {this.renderApplications()}
+            </div>
+        );
     }
 }
 
-export default Applications;
