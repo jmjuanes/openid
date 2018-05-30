594311518f062866528fd2c54b28a5d9c927abdb
alextjd <alextjd1@gmail.com>
2018-05-30 02:29:20 +0000

    Renamed component. Minor changes

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -3,31 +3,30 @@ import {Btn} from "neutrine";
 
 import "./styles.scss";
 
-//Export subhead component
-export default class Subhead extends React.Component {
-    renderBtn() {
-        if (typeof this.props.onBtnClick === "function") {
-            return (
-                <Btn className="subhead-btn" color={this.props.btnColor} onClick={this.props.onBtnClick}>
-                    {this.props.btnText}
-                </Btn>
-            );
-        }
-    }
-
+//Export header component
+export default class Header extends React.Component {
     render() {
-        return (
-            <div className="subhead">
-                <div className="subhead-heading">{this.props.headerText}</div>
-                {this.renderBtn()}
-            </div>
-        );
+        let props = this.props;
+        //Initialize the header components
+        let textComponent = React.createElement("div", {"className": "header-text"}, props.text);
+        let btnComponent = null;
+        //Check if the button listener is provided
+        if (typeof props.onBtnClick === "function") {
+            let btnProps = {
+                "className": "header-btn",
+                "onClick": props.onBtnClick,
+                "color": props.btnColor
+            };
+            btnComponent = React.CreateElement(Btn, btnProps, props.btnText);
+        }
+        //Return the header component
+        return React.createElement("div", {"className": "header"}, textComponent, btnComponent);
     }
 }
 
-//Subhead default props 
-Subhead.defaultProps = {
-    "headerText": "",
+//Header default props 
+Header.defaultProps = {
+    "text": "",
     "btnText": "",
     "btnColor": "success",
     "onBtnClick": null
