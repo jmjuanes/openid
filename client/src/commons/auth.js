df0fd8cc7cf7f8f10399175dc58eed1220fe2530
alextjd <alextjd1@gmail.com>
2018-06-03 13:25:18 +0000

    Added methods to manage the generated authentication token

--- client/src/commons/auth.js
+++ client/src/commons/auth.js
@@ -1,5 +1,9 @@
 let tokenName = "token";
 
+//Check if the token exists
+export function hasToken () {
+    return getToken() !== null;
+}
 //Return the current token
 export function getToken () {
     return localStorage.getItem(tokenName);
@@ -7,18 +11,19 @@ export function getToken () {
 
 //Save the provided token to the local storage
 export function saveToken (token) {
-    //..
+    localStorage.setItem(tokenName, token);
 }
 
 //Delete the token
 export function deleteToken () {
-    //...
+    localStorage.removeItem(tokenName);
 }
 
 //Generate the authentication credentials
 export function generateAuth () {
+    let currentToken = getToken();
     return {
-        "bearer": getToken()
+        "bearer": (currentToken === null) ? "" : currentToken
     };
 }
 
