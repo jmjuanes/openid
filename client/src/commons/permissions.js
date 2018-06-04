c3b856626340a5c81ece768b403b21865de1206d
alextjd <alextjd1@gmail.com>
2018-06-04 12:03:13 +0000

    Added name permission

--- client/src/commons/permissions.js
+++ client/src/commons/permissions.js
@@ -1,5 +1,9 @@
 //List with all the permissions
 let permissions = {
+    "name": {
+        "name": "Your name",
+        "description": "This application will be able to read your name."
+    },
     "email": {
         "name": "Email",
         "description": "This application will be able to read your private email address." 
