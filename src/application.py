e9a81677d6ac5e9ad425a2420577e80e8f90f07a
alextjd <alextjd1@gmail.com>
2018-06-04 12:23:25 +0000

    Added new columns to the applications table. Renamed methods

--- src/application.py
+++ src/application.py
@@ -10,40 +10,41 @@ import string
 class Application(ndb.Model):
     secret = ndb.StringProperty(indexed=False)
     name = ndb.StringProperty(indexed=False)
-    detail = ndb.StringProperty(indexed=False)
-    redirect = ndb.StringProperty(indexed=False)
+    description = ndb.StringProperty(indexed=False)
+    permissions = ndb.StringProperty(indexed=False)
+    homepage_url = ndb.StringProperty(indexed=False)
+    privacy_url = ndb.StringProperty(indexed=False)
+    redirect_url = ndb.StringProperty(indexed=False)
 
 
 # Get an application by id
-def get_application(value):
+def get(value):
     try:
         # Convert the id to integer and return the application
         value = int(value)
         return Application.get_by_id(value)
     except ValueError:
-        # Catch an error: return none and exit
         return None
 
 
-# Extract all the apps
-def getAll():
+# Extract all the applications from the database
+def get_all():
     try:
-        # Get all the applications
         apps = Application.query().fetch()
         return apps
     except ValueError:
-        # Catch an error: return none and exit
         return None
 
 
 # Check if an application exists
-def exists_application(value):
-    return get_application(value) is not None
+def exists(value):
+    return get(value) is not None
 
 
 # Generate a secret for the application
-def generateSecret():
+def generate_secret():
     secret = ''.join(
         random.SystemRandom().choice(string.ascii_lowercase + string.digits + string.ascii_uppercase) for _ in
         range(50))
     return secret
+
