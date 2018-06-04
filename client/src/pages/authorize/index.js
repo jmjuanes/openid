dbb935c1b2fc6fb064910e16cb8c7101cf8f9d3b
alextjd <alextjd1@gmail.com>
2018-06-04 12:01:13 +0000

    Big rewrite

--- client/src/pages/authorize/index.js
+++ client/src/pages/authorize/index.js
@@ -1,178 +1,133 @@
 import React from "react";
-import {Alert, Btn, Field, FieldHelper, FieldLabel, Heading, Input, Spinner} from 'neutrine';
+import {Btn, Field, FieldHelper, FieldLabel, Heading, Small, Spinner} from 'neutrine';
+import {Card, CardTitle, CardBody} from "neutrine";
 import {request} from "@kofijs/request";
+import {redirectHashbang as redirect} from "rouct";
 
 import "./styles.scss";
-import Captcha from "../../components/captcha/index";
 
-class Authorize extends React.Component {
+import * as auth from "../../commons/auth.js";
+import * as notification from "../../commons/notification.js";
+import * as permissions from "../../commons/permissions.js";
+
+//Authorize class
+export default class Authorize extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: null,
-            app: null
+            "app": null,
+            "loading": true
         };
-        this.ref = {
-            emailInput: React.createRef(),
-            pwdInput: React.createRef(),
-            captchaInput: React.createRef()
-        };
-        // Bind functions
-        this.handleAuthorizeClick = this.handleAuthorizeClick.bind(this);
-        this.captchaError = this.captchaError.bind(this);
+        //Bind functions
+        this.handleAuthorization = this.handleAuthorization.bind(this);
     }
 
-    captchaError() {
-        return (
-            <Alert color={"red"} className={"register-error"}>
-                Captcha validation error
-            </Alert>
-        );
-    }
-
-
-    // Authorize and redirect
-    handleAuthorizeClick() {
+    componentDidMount() {
+        //Check the application ID
+        if (typeof this.props.request.query.id !== "string") {
+            return notification.error("No application ID provided");
+        }
+        let id = this.props.request.query.id;
+        //Check if the user is logged in
+        if (auth.hasToken() === false) {
+            let continueUrl = window.encodeURIComponent("/authorize/?id=" + id);
+            return redirect("/login?continueTo=" + continueUrl);
+        }
         let self = this;
-        let credentials = {
-            email: this.ref.emailInput.current.value,
-            pwd: this.ref.pwdInput.current.value,
-            client: this.state.app.id
+        //Import the application information
+        let requestOptions = {
+            "url": "/api/applications/" + id,
+            "method": "get",
+            "json": true
         };
-        // If the captcha is enabled check it
-        if (this.props.captcha_enabled) {
-            credentials = Object.assign({recaptcha: this.ref.captchaInput.current.getResponse()}, credentials);
-        }
-
-        // Check if the email is valid
-        if (credentials.email.indexOf("@") === -1) {
-            return this.setState({error: "Invalid email provided"});
-        }
-        // Check if the password is valid
-        if (credentials.pwd.length < 6) {
-            return this.setState({error: "Invalid password"});
-        }
-        // Check if the application id exists
-        if (typeof credentials.client !== "string" || credentials.client.length === 0) {
-            return this.setState({error: "Invalid application id"});
-        }
-
-        // Do the request to the authorize route
-        request({url: "/api/authorize", method: "post", json: true, body: credentials}, function (err, res, body) {
-            if (err) {
-                return self.setState({error: err.message});
+        return request(requestOptions, function (error, response, body) {
+            if (error) {
+                return notification.error(error.message);
             }
-            if (res.statusCode >= 300) {
-                return self.setState({error: body.message})
+            if (response.statusCode >= 300) {
+                return notification.error(body.message);
             }
-            // The API returns a client token
-            // let url = self.app.redirect + "/test?token=" +body.client_token;
-            let url = "http://localhost:5000" + "/test?token=" + body.client_token;
-            window.location.replace(url);
+            //Save the application information
+            return self.setState({"app": body, "loading": false});
         });
     }
 
-    // Display the error message
-    renderError() {
-        if (this.state.error) {
-            return (
-                <Alert color={"red"} className={"authorize-error"}>
-                    {this.state.error}
-                </Alert>
-            )
-        }
-    }
-
-
-    // Ask for the app info to the API
-    componentDidMount() {
+    //Authorize and redirect
+    handleAuthorization() {
         let self = this;
-        let query = this.props.request.query;
-
-        //Check for no app id provided
-        if (typeof query.id !== "string") {
-            return this.setState({error: "No application ID provided"});
-        }
-        //Check for invalid app id
-        if (query.id.length === 0) {
-            return this.setState({error: "Invalid application ID provided"});
-        }
-        let url = "/api/applications/" + query.id;
-
-        // Do the request to the specific application route
-        request({url: url, method: "get", json: true},
-            function (err, res, body) {
-                if (err) {
-                    return self.setState({error: err.message});
+        let id = this.props.request.query.id;
+        return this.setState({"loading": true}, function () {
+            let requestOptions = {
+                "url": "/api/user/authorizations/" + idi,
+                "method": "post",
+                "json": true,
+                "auth": auth.generateAuth()
+            };
+            //Do the request to the authorize route
+            return request(requestOptions, function (error, response, body) {
+                if (error) {
+                    notification.error(error.message);
+                    return self.setState({"loading": false});
                 }
-                if (res.statusCode >= 300) {
-                    return self.setState({error: body.message});
+                if (response.statusCode >= 300) {
+                    notification.error(body.message);
+                    return self.setState({"loading": false});
                 }
-                return self.setState({
-                    app: {
-                        id: query.id,
-                        name: body.name,
-                        detail: body.detail,
-                        redirect: body.redirect
-                    }
+                console.log(body);
+                //The API returns a client token
+                //let url = self.app.redirect + "/test?token=" +body.client_token;
+                //let url = "http://localhost:5000" + "/test?token=" + body.client_token;
+                //Redirect to the generated url
+                //window.location.replace(url);
+                return self.setState({"loading": false}, function () {
+                    return false;
                 });
             });
+        });
     }
 
+    //Render the permissions list
+    renderPermissions() {
+        let children = [];
+        //Loop over all permissions of the application
+        permissions.getAll().forEach(function (item, index) {
+            let itemTitle = React.createElement(CardTitle, {"className": "pf-authorize-permissions-title"}, item.name);
+            let itemContent = React.createElement(Small, {"className": "pf-authorize-permissions-description"}, item.description);
+            //Save the list item element
+            children.push(React.createElement(CardBody, {"key": index}, itemTitle, itemContent));
+        });
+        //Return the list element
+        return React.createElement(Card, {"className": "pf-authorize-permissions"}, children);
+    }
 
     render() {
-        // Show a loading screen before getting the info from the API
-        if (this.state.app === null) {
-            return(
-                <Spinner className={"authorize-loading"}/>
-            );
+        //Show a loading screen before getting the info from the API
+        if (this.state.loading === true) {
+            return <Spinner color="primary" className="pf-authorize-loading"/>;
         }
-        // Render the actual component
         else {
             return (
-                <div className={"authorize-content"}>
-                    {/*Title*/}
-                    <Heading align={"center"} type={"h2"}>Authorize - {this.props.openid_name}</Heading>
-                    {/*Detail*/}
-                    <div className={"authorize-subtitle"} align={"center"}><b>{this.state.app.name}</b> is requesting
-                        permission to
-                        access your account information.
-                    </div>
-                    {/*Authorize form*/}
-                    <div className={"authorize-form"}>
-                        {/*Error message*/}
-                        {this.renderError()}
-                        {/*Email input*/}
-                        <Field>
-                            <FieldLabel>Email</FieldLabel>
-                            <Input className="authorize-input"
-                                   ref={this.ref.emailInput}
-                                   required/>
-                            <FieldHelper>Please enter a valid email</FieldHelper>
-                        </Field>
-                        {/*Password input*/}
-                        <Field>
-                            <FieldLabel>Password</FieldLabel>
-                            <Input className="authorize-input"
-                                   type={"password"}
-                                   ref={this.ref.pwdInput}
-                                   required/>
-                        </Field>
-                        {/*Captcha*/}
-                        {this.props.captcha_enabled ? (<Captcha sitekey={this.props.captcha_key}
-                                                                onError={this.captchaError}
-                                                                ref={this.ref.captchaInput}/>) : (null)}
-                        {/*Notice*/}
-                        <div className="authorize-privacy siimple-small" align="center">
-                            Check that all the information is correct and click on <b>"Authorize"</b>
-                        </div>
-                        {/*Submit the information*/}
-                        <Btn color={"blue"} onClick={this.handleAuthorizeClick} fluid>Authorize</Btn>
-                    </div>
+                <div className="pf-authorize-content">
+                    <Heading align="center" type="h2">
+                        Authorize {this.state.app.name}
+                    </Heading>
+                    <Small className="pf-authorize-subtitle" align="center">
+                        <strong>{this.state.app.name}</strong> is requesting permission to access the following information of your account:
+                    </Small>
+                    {this.renderPermissions()}
+                    <Small className="pf-authorize-privacy">
+                        By clicking <strong>Authorize {this.state.app.name}</strong>, you allow this application to use your information in accordance with its privacy policy and terms of service. 
+                        You can always revoke the access of this application to your personal information in your dashboard. 
+                    </Small>
+                    <Btn color="primary" className="pf-authorize-submit" onClick={this.handleAuthorization} fluid>
+                        Authorize <strong>{this.state.app.name}</strong>
+                    </Btn>
+                    <Small className="pf-authorize-footer">
+                        After authorizing the application you will be redirected to its website
+                    </Small>
                 </div>
             );
         }
-    };
+    }
 }
 
-export default Authorize;
