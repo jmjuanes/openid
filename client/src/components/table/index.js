0265c28d882c3d7cb33328f91ea0c56540b06668
alextjd <alextjd1@gmail.com>
2018-06-08 15:59:53 +0000

    Big rewrite

--- client/src/components/table/index.js
+++ client/src/components/table/index.js
@@ -10,72 +10,41 @@ let h = React.createElement;
 export default class Table extends React.Component {
     constructor(props) {
         super(props);
-        this.handleActiveClick = this.handleActiveClick.bind(this);
     }
 
-    handleActiveClick(e) {
-        let index = parseInt(e.nativeEvent.target.dataset.index);
-        if (typeof this.props.onActionClick === "function") {
-            this.props.onActionClick.call(null, this.props.data[index], index);
-        }
-    }
-
-    renderItemLogo(item, index) {
-        let logo = h("div", {className: "table-logo table-logo--" + this.props.icon}, null);
-        return h("div", {className: "table-cell"}, logo);
+    //Render the row image
+    renderItemImage(item, index) {
+        return h("div", {"className": "pf-table-cell", "key": 0}, this.getImage.call(this, item, index));
     }
 
     renderItemContent(item, index) {
-        let title = h("div", {className: "table-title"}, this.props.customTitle.call(null, item));
-        let detail = h("div", {className: "table-detail"}, this.props.customDetail.call(null, item));
+        //Get the item title and description
+        let title = h("div", {"className": "pf-table-title"}, this.getTitle.call(this, item, index));
+        let description = h("div", {"className": "pf-table-description"}, this.getDescription.call(this, item, index));
         //Return the item content
-        return h("div", {className: "table-cell table-cell--primary"}, title, detail);
+        return h("div", {"className": "pf-table-cell pf-table-cell--primary", "key": 1}, title, description);
     }
 
-    renderItemAction(item, index) {
-        let self = this;
-        let btnProps = {
-            "color": this.props.actionColor,
-            "style": {
-                "height": "30px",
-                "lineHeight": "30px",
-                "fontSize": "14px"
-            },
-            "data-index": index.toString(),
-            "onClick": self.handleActiveClick
-        };
-        let button = h(Btn, btnProps, this.props.actionText);
-        return h("div", {className: "table-cell"}, button);
+    //Render the extra columns
+    renderExtraColumns(item, index) {
+        return this.getExtraColumns.call(this, item, index);
     }
 
     render() {
         let self = this;
         //Display all the items of the table
         let children = this.props.data.map(function (item, index) {
-            let itemContent = self.renderItemContent(item, index);
-            let itemLogo = self.renderItemLogo(item, index);
-            let itemAction = self.renderItemAction(item, index);
-            let itemProps = {
-                "className": "table-item",
-                "key": index
-            };
+            //Initialize the columns list
+            let columns = [];
+            columns.push(self.renderItemImage(item, index));
+            columns.push(self.renderItemContent(item, index));
+            //Get the extra columns
+            let extraCols = self.renderExtraColumns(item, index);
             //Return the item element
-            return h("div", itemProps, itemLogo, itemContent, itemAction);
+            return h("div", {"className": "pf-table-item", "key": index}, columns.concat(extraCols));
         });
         //Return the applications table
-        return h("div", {className: "table"}, children);
+        return h("div", {"className": "pf-table"}, children);
     }
 }
-
-//Table default props
-Table.defaultProps = {
-    data: [],
-    icon: "applications",
-    customTitle: null,
-    customDetail: null,
-    onClick: null,
-    onActionClick: null,
-    actionText: "Manage",
-    actionColor: "primary"
-};
 
