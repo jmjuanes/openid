e16cf0adb3aff2f527912a7632a42b76a44b2b3b
alextjd <alextjd1@gmail.com>
2018-06-11 13:09:09 +0000

    Minor changes

--- src/captcha.py
+++ src/captcha.py
@@ -8,19 +8,16 @@ from google.appengine.api import urlfetch
 # Verify captcha
 def verify(captcha_response, captcha_secret, captcha_url):
     # Check for empty captcha response
-    if captcha_response is '':
+    if captcha_response is '' or captcha_response is None:
         return False
-
     # Perform the request to the captcha server
     try:
         form_fields = {'secret': captcha_secret, 'response': captcha_response}
         form_data = urllib.urlencode(form_fields)
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
         # Perform the request
         result = urlfetch.fetch(url=captcha_url, payload=form_data, method=urlfetch.POST, headers=headers)
         # logging.info('Captcha response code: ' + str(result.status_code) + '')
-
         # Check the result status code
         if result.status_code == 200:
             data = json.loads(result.content)
@@ -31,3 +28,4 @@ def verify(captcha_response, captcha_secret, captcha_url):
     except urlfetch.Error:
         logging.exception('Caught exception fetching url for verify captcha')
         return False
+
