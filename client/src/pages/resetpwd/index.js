07042e9f972aef572fed634b9221c024123e9572
alextjd <alextjd1@gmail.com>
2018-06-12 13:26:32 +0000

    Init resetpwd view

---
+++ client/src/pages/resetpwd/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Heading, Small} from "neutrine";
+import * as Router from "rouct";
+
+import * as notification from "../../commons/notification.js";
+
+import ResetPwdSend from "./send.js";
+import ResetPwdReset from "./reset.js";
+
+import "./styles.scss";
+
+//Export resetpwd component
+export default class ResetPwd extends React.Component {
+    render() {
+        return (
+            <div className="pf-resetpwd-content">
+                <Heading type="h2" align="center">Reset password</Heading>
+                <Router.Switch>
+                    <Router.Route exact path="/resetpwd" component={ResetPwdSend}/>
+                    <Router.Route exact path="/resetpwd/:id" component={ResetPwdReset}/>
+                </Router.Switch>
+            </div>
+        );
+    }
+}
+
