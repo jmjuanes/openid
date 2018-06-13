1db0ab01b3e41fde2e38f869680fac0681f333c5
alextjd <alextjd1@gmail.com>
2018-06-13 12:53:30 +0000

    Import captcha configuration

--- src/captcha.py
+++ src/captcha.py
@@ -1,22 +1,29 @@
+# Import python libs
 import logging
 import urllib
 import json
 
+# Import App engine API
 from google.appengine.api import urlfetch
 
+# Import configuration
+import config
 
 # Verify captcha
-def verify(captcha_response, captcha_secret, captcha_url):
+def verify(captcha_response):
     # Check for empty captcha response
     if captcha_response is '' or captcha_response is None:
         return False
     # Perform the request to the captcha server
     try:
-        form_fields = {'secret': captcha_secret, 'response': captcha_response}
+        form_fields = {
+            'secret': config.captcha_secret_key, 
+            'response': captcha_response
+        }
         form_data = urllib.urlencode(form_fields)
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         # Perform the request
-        result = urlfetch.fetch(url=captcha_url, payload=form_data, method=urlfetch.POST, headers=headers)
+        result = urlfetch.fetch(url=config.captcha_url, payload=form_data, method=urlfetch.POST, headers=headers)
         # logging.info('Captcha response code: ' + str(result.status_code) + '')
         # Check the result status code
         if result.status_code == 200:
