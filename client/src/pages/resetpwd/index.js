5b0f2d57d9c59410127a23507726f26c3609ecf4
alextjd <alextjd1@gmail.com>
2018-06-13 12:29:30 +0000

    Pass captcha configuration to children routes

--- client/src/pages/resetpwd/index.js
+++ client/src/pages/resetpwd/index.js
@@ -12,12 +12,16 @@ import "./styles.scss";
 //Export resetpwd component
 export default class ResetPwd extends React.Component {
     render() {
+        let props = {
+            "captcha_enabled": this.props.captcha_enabled,
+            "captcha_key": this.props.captcha_key
+        };
         return (
             <div className="pf-resetpwd-content">
                 <Heading type="h2" align="center">Reset password</Heading>
                 <Router.Switch>
-                    <Router.Route exact path="/resetpwd" component={ResetPwdSend}/>
-                    <Router.Route exact path="/resetpwd/:id" component={ResetPwdReset}/>
+                    <Router.Route exact path="/resetpwd" component={ResetPwdSend} props={props}/>
+                    <Router.Route exact path="/resetpwd/:id" component={ResetPwdReset} props={props}/>
                 </Router.Switch>
             </div>
         );
