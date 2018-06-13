dfb05984127058386598ceda9e0d83e5c5de868d
alextjd <alextjd1@gmail.com>
2018-06-13 11:00:22 +0000

    Rewrite configuration file

--- config.sample.py
+++ config.sample.py
@@ -3,52 +3,30 @@
 # your own configuration.
 # Check the documentation at https://github.com/mgviz/passfort
 
-# PassFort configuration
-# ======================
+# PassFort gobal configuration
+global_name = 'PassFort' # Oauth name
+global_email = 'APP_ID@@appspot.gserviceaccount.com' # Authorized address to send emails
+global_public_key = 'passfort' # Public key string
+global_secret_key = 'PASSFORT_SECRET_KEY' # Secret key string
 
-# PassFort visible name
-passfort_name = 'PassFort'
+# Signup configuration
+signup_enabled = True # Allow users to create a new account
+signup_default_active = True # New user is active by default
+signup_default_admin = False # New user is admin by default
 
-# PassFort key value
-passfort_key = 'passfort'
-
-# PassFort secret key
-passfort_secret = 'PASSFORT_SECRET_KEY'
-
-# Default active value
-passfort_default_active = True
-
-# Allow signup: allow users to create a new account
-passfort_allow_signup = False
-
-# Allow reset password: allow users to reset his password
-passfort_allow_resetpwd = False
-
+# ResetPwd configuratiom
+resetpwd_enabled = True # Allow users to reset his password
+resetpwd_expiration = 24 # Number of hours where the resetpwd id is valid
 
 # Captcha configuration
-# ======================
 # Visit the ReCaptcha documentation at https://developers.google.com/recaptcha/intro
-
-# Captcha enabled: force to use the captcha when login or register
-captcha_enabled = False
-
-# Captcha verification url (you should not change this value)
-captcha_url = 'https://www.google.com/recaptcha/api/siteverify'
-
-# Captcha key: your captcha key
-captcha_key = 'YOUR_CAPTCHA_KEY'
-
-# Captcha secret key: your captcha secret key value
-captcha_secret = 'YOUR_CAPTCHA_SECRET_KEY'
-
+captcha_enabled = False # force to use the captcha when login or register 
+captcha_public_key = 'YOUR_CAPTCHA_KEY' # Captcha public key
+captcha_secret_key = 'YOUR_CAPTCHA_SECRET_KEY' # Captcha secret key
+captcha_url = 'https://www.google.com/recaptcha/api/siteverify' # Captcha verification url
 
 # Token configuration
-# ======================
 # Read more about JSON web tokens: https://en.wikipedia.org/wiki/JSON_Web_Token
-
-# Token expiration days: number of days where the token is valid
-token_expiration = 7 * (24 * 60 * 60)
-
-# Token sign algorithm: allowed HS256 or RS256
-token_algorithm = 'HS256'
+token_expiration = 7 * (24 * 60 * 60) # number of days where the token is valid
+token_algorithm = 'HS256' # Token sign algorithm: allowed HS256 or RS256
 
